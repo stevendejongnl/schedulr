b9d14989da7addc34fa69fb5023e343275293590
Steven de jong 🍟 <steven@steven-dejong.nl>
2024-06-20 21:02:07 +0200

    Add app header

---
+++ src/components/header.ts
@@ -0,0 +1,10 @@
+import { LitElement, html } from "lit"
+import { customElement } from "lit/decorators.js"
+
+@customElement('schedulr-header')
+export class SchedulrHeader extends LitElement {
+  override render() {
+    return html`<h1>Schedulr</h1>`
+  }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,2 @@
-import { LitElement, html } from "lit"
-import { customElement } from "lit/decorators.js"
-
-@customElement('schedulr-app')
-export class SchedulrApp extends LitElement {
-  override render() {
-    return html`<h1>Schedulr</h1>`
-  }
-}
+export * from 'app.js'
+export * from 'components/main.js'
